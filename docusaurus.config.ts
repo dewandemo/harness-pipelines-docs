c089a0ed967367e2f128635333de5a619ce643b3
Dewan Ahmed <dewan.ahmed@harness.io>
2025-06-26 17:26:46 -0300

    Update link.

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -79,7 +79,7 @@ const config: Config = {
           items: [
             {
               label: 'Tutorial',
-              to: '/docs/intro',
+              to: '/docs/getting-started',
             },
           ],
         },
